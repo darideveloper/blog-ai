512f3257d67d756a98fddfd727cff3e7c586a89c
darideveloper <darideveloper@gmail.com>
2023-03-30 16:23:29 -0600

    feat (components/ButtonMenu.jsx) interactive menu button

---
+++ src/components/ButtonMenu.jsx
@@ -0,0 +1,33 @@
+import Button from './Button'
+
+export default function ButtonMenu({ }) {
+
+
+  return (
+    <Button
+      extra_classes='w-10 h-10 mt-2 sm:hidden menu-button'
+      active_style={{ transform: 'rotate(180deg)' }}
+      on_click={() => {
+
+        // Activate header
+        const header = document.querySelector('header')
+        header.classList.toggle('active')
+      }}
+    >
+      <svg
+        clip-rule='evenodd'
+        fill-rule='evenodd'
+        stroke-linejoin='round'
+        stroke-miterlimit='2'
+        viewBox='0 0 24 24'
+        xmlns='http://www.w3.org/2000/svg'
+        class='fill-slate-100'
+      >
+        <path
+          d='m16.843 10.211c.108-.141.157-.3.157-.456 0-.389-.306-.755-.749-.755h-8.501c-.445 0-.75.367-.75.755 0 .157.05.316.159.457 1.203 1.554 3.252 4.199 4.258 5.498.142.184.36.29.592.29.23 0 .449-.107.591-.291 1.002-1.299 3.044-3.945 4.243-5.498z'
+        >
+        </path>
+      </svg>
+    </Button >
+  )
+}
