49e5e9a46c661af47a1ce0ecc6a1131a1cdc9db8
darideveloper <darideveloper@gmail.com>
2023-04-07 20:57:58 -0600

    feat (cms) install netlify cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,28 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
-
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
+import tailwind from "@astrojs/tailwind"
+import react from "@astrojs/react"
+// import NetlifyCMS from 'astro-netlify-cms'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), react()]
-});
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    react(),
+    // NetlifyCMS({
+    //   config: {
+    //     backend: {
+    //       name: 'git-gateway',
+    //       branch: 'master',
+    //     },
+    //     collections: [
+    //       // Content collections
+    //     ],
+    //   },
+    // })
+  ]
+})
