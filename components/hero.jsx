0c8e1425cb2bf0e8c5ec736baa44ffd8c0b00445
daridev <darideveloper@gmail.com>
2023-06-28 10:50:13 -0600

    feat (hero): layout and responsive

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,10 +1,16 @@
 import { metadata } from "@/lib/metadata"
 
+import { titleFont } from "@/lib/fonts"
+
 export default function Hero() {
   return (
-    <section className='hero' >
-      <h1>Dari Dev Blog</h1>
-      <p>
+    <section className='hero container mx-auto my-10' >
+      <p 
+        className={`
+          text-2xl text-center text-white
+          ${titleFont.className}
+        `}
+      >
         {metadata.description}
       </p>
     </section>
