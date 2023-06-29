fcc8ef846a77cf65d21e428b9e08c598c83bb5e7
daridev <darideveloper@gmail.com>
2023-06-29 14:15:59 -0600

    feat (hero): add h1

--- components/hero.jsx
+++ components/hero.jsx
@@ -5,14 +5,14 @@ import { titleFont } from "@/lib/fonts"
 export default function Hero() {
   return (
     <section className='hero container mx-auto my-10' >
-      <p 
+      <h1
         className={`
           text-2xl text-center text-white px-2
           ${titleFont.className}
         `}
       >
         {metadata.description}
-      </p>
+      </h1>
     </section>
   )
 }
