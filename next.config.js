e864c046e2edba71285bca943100875ffb0e2fe0
daridev <darideveloper@gmail.com>
2023-07-16 08:41:19 -0600

    feat (settings): update calendar links

--- next.config.js
+++ next.config.js
@@ -15,6 +15,16 @@ const nextConfig = {
         destination: '/',
         permanent: true,
       },
+      {
+        source: '/flask-calendar',
+        destination: 'https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=Mm43Z2hvbDRlZDc2N2E4Y3IwYjFmM21rNjNfMjAyMzA3MTdUMDEwMDAwWiA2Y2YxdDhpams2Mjh1a2ppam1jamIyMGhkMEBn&tmsrc=6cf1t8ijk628ukjijmcjb20hd0%40group.calendar.google.com',
+        permanent: true,
+      },
+      {
+        source: '/git-calendar',
+        destination: 'https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=MG0xcW8xYThtMjdza2Y0NnM1MnI0ampyODYgNmNmMXQ4aWprNjI4dWtqaWptY2piMjBoZDBAZw&tmsrc=6cf1t8ijk628ukjijmcjb20hd0%40group.calendar.google.com',
+        permanent: true,
+      },
     ]
   },
   sassOptions: {
